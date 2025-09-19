aa9dd603be8bf7f94dc9f2673785525b257fc3db
Marwan bouhamid <marwanbouhamid123@gmail.com>
2025-09-19 09:17:05 +0100

    i want to pull some changes

--- exercice.js
+++ exercice.js
@@ -1,13 +1,34 @@
-let tableau = ["C1","C1", "C3", "C2", "C1", "C8", "C2", "C1"];
+// let tableau = ["C1","C1", "C3", "C2", "C1", "C8", "C2", "C1"];
 
-let code = "C2"
+// // Object to count each code
+// let counts = {};
 
-let counter = 0;
+// // Loop through the array
+// for (let i = 0; i < tableau.length; i++) {
+//     let code = tableau[i];
 
-for (let i = 0; i <= tableau.length ; i++){
-    if (tableau[i] === code){
-        counter++;
+
+//     // Count how many times each code appears
+//     if (counts[code]) {
+//         counts[code]++;
+//     } else {
+//         counts[code] = 1;
+//     }
+// }
+
+// for (let code in counts) {
+//     console.log(code + " appears " + counts[code] + " times");
+// }
+
+
+let myFriends = [1,2,3,3 , "AbdeLadim" , "Hitam" , "Ayman" , "Mouhamid" , "Marwan" ,];
+
+let onlyNames = [];
+
+for (let index = 0; index < myFriends.length; index++) {
+    if (typeof myFriends[index] === "string"){
+        onlyNames.push(myFriends[index])
     }
-} 
-
-console.log("the code " + code + " appears "+ counter);
+    
+}
+console.log(onlyNames)
--- exercice2.js
+++ exercice2.js
@@ -1,22 +1,17 @@
- let autoformations = [
-     {nom:"HTML & CSS", code : "C1"},
-     {nom:"Javascript", code : "C2"},
-     {nom:"Git & Github", code : "C3"},
- ];
-
-let question = autoformations[1];
-
-let response ="C2";
- 
-
-if (response === question.code){
-    console.log("true");
-}else {
-    console.log("false the true answer is " + question.code);
-}
+  let autoformations = [
+      {nom:"HTML & CSS", code : "C1"},
+      {nom:"Javascript", code : "C2"},
+      {nom:"Git & Github", code : "C3"},
+  ]
+ let question = autoformations[1]
+ let response ="C1";
+ if (response === question.code){
+     console.log("true");
+ }else {
+  console.log("false the true answer is " + question.code);
+ }
 
 
-
-
+console.log(autoformations[0].code);
 
   
